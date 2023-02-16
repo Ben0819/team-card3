834c9470c30f6718262127fa8a8b146d87945e65
Ben0819 <116129299+Ben0819@users.noreply.github.com>
2023-02-16 13:06:21 -0500

    ret

--- team-card3/src/team-card3.js
+++ team-card3/src/team-card3.js
@@ -1,88 +1,202 @@
-import { LitElement, html, css } from 'lit';
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, css } from "lit";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
-class TeamCard3 extends LitElement {
-  static properties = {
-    header: { type: String },
+/**
+ * `meme-maker`
+ * Meme all the things
+ *
+ * @demo demo/index.html
+ * @microcopy - the mental model for this element
+ *  - go forth and make dank memes yo
+ * @element meme-maker
+ */
+class MemeMaker extends LitElement {
+  static get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+        }
+        * {
+          box-sizing: border-box;
+        }
+        figure {
+          position: relative;
+          width: 100%;
+          margin: 0;
+          padding: 0;
+          font-size: 20px;
+          overflow: hidden;
+        }
+        img {
+          width: 100%;
+          height: auto;
+        }
+        .top-text,
+        .bottom-text {
+          position: absolute;
+          left: 0;
+          width: 100%;
+          padding: 3% 2%;
+          text-align: center;
+          text-transform: uppercase;
+          font-weight: 900;
+          font-family: "Impact", "Arial Black", "sans serif";
+          line-height: 1.2;
+          font-size: var(--meme-maker-font-size, 36px);
+          color: white;
+          text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+            1px 1px 0 #000;
+          letter-spacing: 2px;
+        }
+        .top-text {
+          top: 0;
+        }
+        .bottom-text {
+          bottom: 0;
+        }
+        @media (max-width: 800px) {
+          .top-text,
+          .bottom-text {
+            font-size: var(--meme-maker-font-size-medium, 20px);
+          }
+        }
+        @media (max-width: 600px) {
+          .top-text,
+          .bottom-text {
+            font-size: var(--meme-maker-font-size-small, 20px);
+          }
+        }
+      `,
+    ];
   }
-
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--team-card3-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
-
-  constructor() {
-    super();
-    this.header = 'My app';
-  }
-
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/TeamCard3.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <figure>
+        <img
+          loading="lazy"
+          src="${this.imageUrl}"
+          alt="${this.alt || ""}"
+          aria-describedby="${this.describedBy || ""}"
+        />
+        <figcaption class="top-text">${this.topText}</figcaption>
+        <figcaption class="bottom-text">${this.bottomText}</figcaption>
+      </figure>
     `;
   }
+  static get tag() {
+    return "meme-maker";
+  }
+  constructor() {
+    super();
+    this.alt = "";
+  }
+  static get properties() {
+    return {
+      /**
+       * Alt data passed down to appropriate tag
+       */
+      alt: {
+        type: String,
+      },
+      /**
+       * Aria-describedby data passed down to appropriate tag
+       */
+      describedBy: {
+        type: String,
+        attribute: "described-by",
+      },
+      /**
+       * url to the meme image
+       */
+      imageUrl: {
+        type: String,
+        attribute: "image-url",
+        reflect: true,
+      },
+      /**
+       * Text on top of the image.
+       */
+      topText: {
+        type: String,
+        attribute: "top-text",
+        reflect: true,
+      },
+      /**
+       * Bottom text for the image.
+       */
+      bottomText: {
+        type: String,
+        attribute: "bottom-text",
+        reflect: true,
+      },
+    };
+  }
+  /**
+   * Implements haxHooks to tie into life-cycle if hax exists.
+   */
+  haxHooks() {
+    return {
+      activeElementChanged: "haxactiveElementChanged",
+      progressiveEnhancement: "haxprogressiveEnhancement",
+      gizmoRegistration: "haxgizmoRegistration",
+    };
+  }
+  /**
+   * Supply translations for the UI elements of HAX in meme-maker
+   */
+  haxgizmoRegistration(store) {
+    window.dispatchEvent(
+      new CustomEvent("i18n-manager-register-element", {
+        detail: {
+          namespace: `meme-maker.haxProperties`,
+          localesPath: new URL(`./locales`, import.meta.url).href,
+          locales: ["es", "fr"],
+        },
+      })
+    );
+  }
+  /**
+   * double-check that we are set to inactivate click handlers
+   * this is for when activated in a duplicate / adding new content state
+   */
+  haxactiveElementChanged(el, val) {
+    let figures = this.shadowRoot.querySelectorAll("figcaption");
+    if (val) {
+      for (var i = 0; i < figures.length; i++) {
+        figures[i].setAttribute("contenteditable", true);
+      }
+    } else {
+      for (var i = 0; i < figures.length; i++) {
+        figures[i].removeAttribute("contenteditable");
+      }
+      // easy, name is flat
+      this.topText = this.shadowRoot.querySelector(".top-text").innerText;
+      this.bottomText = this.shadowRoot.querySelector(".bottom-text").innerText;
+    }
+    return false;
+  }
+  /**
+   * Hook for HAX to support progressive enhancement and return a string
+   * to place in the slot of this tag for RSS, bots and legacy formats
+   */
+  haxprogressiveEnhancement(el) {
+    return `
+    ${this.topText ? `<div>${this.topText}</div>` : ""}
+      <img src="${this.imageUrl}" alt="${
+      this.alt
+    }" preload="lazy" aria-describedby="${this.describedBy || ""}"/>
+    ${this.bottomText ? `<div>${this.bottomText}</div>` : ""}`;
+  }
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/meme-maker.haxProperties.json`, import.meta.url).href;
+  }
 }
-
-customElements.define('team-card3', TeamCard3);
+customElements.define(MemeMaker.tag, MemeMaker);
+export { MemeMaker };
